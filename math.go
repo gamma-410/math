176b4d9a7aff3817c747e9fbab20bd987253e14f
gamma-410 <mail@gamma410.win>
2022-09-13 00:28:33 +0900

    ファイルを一行ずつスライスに追加

---
+++ math.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "flag"
+  "bufio"
+)
+
+func main() {
+  flag.Parse()
+  name := flag.Arg(0)
+  f, _ := os.Open(name)
+  scanner := bufio.NewScanner(f)
+
+  lenNum := 0
+  capNum := 0
+  cutText := make([]string, lenNum, capNum)
+
+  i := 0
+  for scanner.Scan() {
+    text := scanner.Text()
+    cutText = append(cutText, text)
+    i++
+  }
+  fmt.Println(cutText)
+}
